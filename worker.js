68c34893c6bfeb79a141897590277af3a2986459
Sam Carlton <sam@sam.lc>
2022-02-10 15:28:03 -0600

    Allow all origins

--- worker.js
+++ worker.js
@@ -14,9 +14,12 @@ async function handleRequest(request) {
 
     return new Response(JSON.stringify( coords ), {
         headers: {
-          'content-type': 'application/json',
-          // https://developers.cloudflare.com/cache/about/cache-control#examples,
-          'Cache-Control': 'no-store, no-cache, max-age=0, s-maxage=0',
+            'content-type': 'application/json',
+            // https://developers.cloudflare.com/cache/about/cache-control#examples,
+            'Cache-Control': 'no-store, no-cache, max-age=0, s-maxage=0',
+
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
         },
     })
 }
