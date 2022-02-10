ba4c8b54804b377a0558b714e7b2dcd2aa0c9036
Sam Carlton <sam@sam.lc>
2022-02-10 13:51:56 -0600

    Respond with coords

--- worker.js
+++ worker.js
@@ -1,3 +1,20 @@
-addEventListener("fetch", (event) => {
-    event.respondWith(new Response("Hello Miniflare!"));
+addEventListener('fetch', (event) => {
+    event.respondWith( handleRequest(event.request) );
 });
+
+
+async function handleRequest(request) {
+    latitude = request.cf.latitude
+    longitude = request.cf.longitude
+
+    const coords = {
+        latitude,
+        longitude
+    }
+
+    return new Response(JSON.stringify( coords ), {
+        headers: {
+          'content-type': 'application/json',
+        },
+    })
+}
